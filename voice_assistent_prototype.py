6206aa2f7e7cdc79f96ada88371bdff7d4059a54
Ilmar Junior <imajuninho@gmail.com>
2023-12-22 12:57:19 -0300

    feat: activate by voice only

--- voice_assistent_prototype.py
+++ voice_assistent_prototype.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 import textwrap
 import speech_recognition as sr
 from openai import OpenAI
@@ -33,12 +34,10 @@ def play_audio(file_path):
 
 def speech_to_text(audio):
     try:
-        print("Processando...")
         text = rec.recognize_google(audio, language=language)
         print(text)
         return text
     except:
-        print('Falha no reconhecimento de voz')
         return ""
 
 
@@ -59,63 +58,100 @@ def consult_chatGPT(query):
     response = client.chat.completions.create(
         model="gpt-4-1106-preview",
         messages=[
-            {"role": "system", "content": "Você é um assistente pessoal chamado Cleiton extremamente habilidoso e "
-                                          "inteligente em diversas áreas do conhecimento"},
+            {"role": "system", "content": "Você é um assistente pessoal de voz chamado Cleiton, extremamente "
+                                          "habilidoso e inteligente em diversas áreas do conhecimento"},
+            {"role": "system", "content": "Você é um assistente que está interessado em conversar sobre o tema das "
+                                          "perguntas. Por isso sempre pergunte o que mais a pessoa gostaria de saber"
+                                          " ou estimule ela a falar mais sobre..."},
             {"role": "user", "content": query}
         ]
     )
-
     print("Pensando...")
     chat_answer = response.choices[0].message.content
     return chat_answer
 
 
-def make_a_question():
+def make_a_question(text):
+    response = consult_chatGPT(text)
+    text_to_speech(response, 1.0)
     with sr.Microphone() as mic:
-        try:
-            rec.adjust_for_ambient_noise(mic)
-            print("Ouvindo...")
-            audio = rec.listen(mic, timeout=5, phrase_time_limit=10)
-            text = speech_to_text(audio)
-            if not text:
-                response = consult_chatGPT(text)
-                text_to_speech(response, 1.0)
-                print("Ouvindo...")
-                audio = rec.listen(mic)
-                text = speech_to_text(audio)
-            response = consult_chatGPT(text)
-            text_to_speech(response, 1.0)
-        except sr.WaitTimeoutError:
-            pass
+        rec.adjust_for_ambient_noise(mic)
+        while True:
+            try:
+                    print("Ouvindo...")
+                    audio = rec.listen(mic, timeout=5 , phrase_time_limit=10)
+                    print("Processando")
+                    text = speech_to_text(audio)
+                    if "cleiton sair" in text.lower():
+                        text_to_speech("Saindo...")
+                        sys.exit()
+                    elif "tchau cleiton" in text.lower():
+                        text_to_speech("Tchau, estarei por aqui se precisar...")
+                        break
+                    if not text:
+                        response = consult_chatGPT(text)
+                        text_to_speech(response, 1.0)
+                        print("Ouvindo...")
+                        audio = rec.listen(mic)
+                        print("Processando")
+                        text = speech_to_text(audio)
+                        if "cleiton sair" in text.lower():
+                            text_to_speech("Saindo...")
+                            sys.exit()
+                        elif "tchau cleiton" in text.lower():
+                            text_to_speech("Tchau, estarei por aqui se precisar...")
+                            break
+                    response = consult_chatGPT(text)
+                    text_to_speech(response, 1.0)
+            except sr.WaitTimeoutError:
+                pass
 
 
-def menu():
-    menu = """\n
-    ================ MENU ================
-    [1]\tPerguntar
-    [2]\tSair
-    => """
-    return input(textwrap.dedent(menu))
+# def menu():
+#     menu = """\n
+#     ================ MENU ================
+#     [1]\tPerguntar
+#     [2]\tSair
+#     => """
+#     return input(textwrap.dedent(menu))
 
 
 def main():
-    text_to_speech("\nComo posso ajudar?")
-    while True:
-        text_to_speech("Digite 1 para perguntar e digite 2 para sair.")
-        option = menu()
+    with sr.Microphone() as mic:
+        rec.adjust_for_ambient_noise(mic)
+        print("Cleiton Online")
+        while True:
+            try:
+                    audio = rec.listen(mic, timeout=5, phrase_time_limit=10)
+                    text = speech_to_text(audio)
+                    if "e aí cleiton" in text.lower():
+                        try:
+                            make_a_question(text)
+                        except Exception:
+                            pass
+                    elif "cleiton sair" in text.lower():
+                        text_to_speech("Saindo...")
+                        sys.exit()
+            except sr.WaitTimeoutError:
+                pass
 
-        match option:
-            case "1":
-                try:
-                    make_a_question()
-                except Exception:
-                    pass
-            case "2":
-                text_to_speech("Saindo...")
-                break
-            case _:
-                print("")
-                text_to_speech("Opção inválida, tente novamente...")
+
+# while True:
+#     text_to_speech("Digite 1 para perguntar e digite 2 para sair.")
+#     option = menu()
+#
+#     match option:
+#         case "1":
+#             try:
+#                 make_a_question()
+#             except Exception:
+#                 pass
+#         case "2":
+#             text_to_speech("Saindo...")
+#             break
+#         case _:
+#             print("")
+#             text_to_speech("Opção inválida, tente novamente...")
 
 
 main()
